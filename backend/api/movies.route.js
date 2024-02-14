132e5a988b00742cb2cb115cbbab728847cfda5e
Jose <josehneves28@gmail.com>
2024-02-14 09:57:30 -0500

    created the reviews apis and dao

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,6 +1,13 @@
 import express from "express";
+import MoviesController from "./movies.controller.js";
+import ReviewsController from "./reviews.controller.js";
+
 const router = express.Router();
-router.route("/").get((req, res) => {
-  res.send("Hello World");
-});
+
+router.route("/").get(MoviesController.apiGetMovies);
+router.route("/review").post(ReviewsController.apiPostReview);
+router.route("/review").put(ReviewsController.apiUpdateReview);
+router.route("/review").delete(ReviewsController.apiDeleteReview);
+
+
 export default router;
--- backend/dao/moviesDAO.js
+++ backend/dao/moviesDAO.js
@@ -13,42 +13,42 @@ export default class MoviesDAO {
       );
     }
   }
-}
 
-async function getMovies({
-  filters = null,
-  page = 0,
-  moviesPerPage = 20,
-} = {}) {
-  let query;
-  if (filters) {
-    if ("title" in filters) {
-      query = { $text: { $search: filters["title"] } };
-    } else if ("rated" in filters) {
-      query = { rated: { $eq: filters["rated"] } };
-    } else if ("genre" in filters) {
-      query = { genre: { $eq: filters["genre"] } };
-    } else if ("year" in filters) {
-      query = { year: { $eq: filters["year"] } };
-    } else if ("director" in filters) {
-      query = { director: { $eq: filters["director"] } };
-    } else if ("actor" in filters) {
-      query = { actor: { $eq: filters["actor"] } };
-    } else if ("runtime" in filters) {
-      query = { runtime: { $eq: filters["runtime"] } };
+  static async getMovies({
+    filters = null,
+    page = 0,
+    moviesPerPage = 20,
+  } = {}) {
+    let query;
+    if (filters) {
+      if ("title" in filters) {
+        query = { $text: { $search: filters["title"] } };
+      } else if ("rated" in filters) {
+        query = { rated: { $eq: filters["rated"] } };
+      } else if ("genre" in filters) {
+        query = { genre: { $eq: filters["genre"] } };
+      } else if ("year" in filters) {
+        query = { year: { $eq: filters["year"] } };
+      } else if ("director" in filters) {
+        query = { director: { $eq: filters["director"] } };
+      } else if ("actor" in filters) {
+        query = { actor: { $eq: filters["actor"] } };
+      } else if ("runtime" in filters) {
+        query = { runtime: { $eq: filters["runtime"] } };
+      }
     }
-  }
-  let cursor;
-  try {
-    cursor = await movies
-      .find(query)
-      .limit(moviesPerPage)
-      .skip(moviesPerPage * page);
-    const moviesList = await cursor.toArray();
-    const totalNumMovies = await movies.countDocuments(query);
-    return { moviesList, totalNumMovies };
-  } catch (e) {
-    console.error(`Unable to issue find command, ${e}`);
-    return { moviesList: [], totalNumMovies: 0 };
+    let cursor;
+    try {
+      cursor = await movies
+        .find(query)
+        .limit(moviesPerPage)
+        .skip(moviesPerPage * page);
+      const moviesList = await cursor.toArray();
+      const totalNumMovies = await movies.countDocuments(query);
+      return { moviesList, totalNumMovies };
+    } catch (e) {
+      console.error(`Unable to issue find command, ${e}`);
+      return { moviesList: [], totalNumMovies: 0 };
+    }
   }
 }
